01ceb182e389fd1d0f455565eb83d26a5a1fae25
shunwatai <shunwatai@gmail.com>
2016-02-20 14:32:47 +0800

    newly added, may have bugs

--- sfs_functions/read_t.c
+++ sfs_functions/read_t.c
@@ -5,51 +5,62 @@
 #include <fcntl.h>     // for open/read/write
 #include <unistd.h>    // for lseek
 #include <string.h>    // for strlen
+#include <stdlib.h>
 
 #include "../sfs.h"    // SFS structures provided by ar sir
 #include "sys_call.h"  // header file that included this read_t for user commands
 
-/* i dont know how to use the "offset". I assume it is 0 sin which read at start of data blk. 
+/* inode_number & offset for the location of the file. buf is the space for content and count is bytes size.
  * This function for user command cat_t */
 int read_t( int inode_number, int offset, void *buf, int count){ 
+    //printf("inode#: %d\ndata_offset: %d\nsizeof(buf): %lu\ncount: %d\n",inode_number,offset,sizeof(buf),count);
     ssize_t ret = 0; // get the bytes of read
-    
+        
     /* open HD */
     int fd; // for open HD to read
     fd = open("../HD",O_RDONLY); // open ../HD for readonly
-    
+
     /* seek to inode according to "inode_number" parameter start at INODE_OFFSET */
-    int inode_offset = INODE_OFFSET + (INODE_OFFSET * inode_number);
-    lseek(fd, inode_offset, SEEK_SET); // seek to inode_offset
+    int inode_offset = INODE_OFFSET + sizeof(struct inode) * inode_number; // get the inode# offset
+    lseek(fd, inode_offset, SEEK_SET); // seek to inode_offset (the target file to read)
     
-    /* get inode info. inorder to get  */
-    struct inode inodes;
+    /* get inode info. in order to get file bytes size + data blk offset */
+    struct inode inodes = {};
     ret = read(fd, &inodes, sizeof(struct inode)); // read inode info to inodes
     if(ret!=sizeof(struct inode)){
         perror("failed to read inode");
         return -1;
     }
-    int file_size = inodes.i_size; // get file size
+    if(inodes.i_type!=1){ // not regular file
+        printf("did you enter the dir???\n");
+        return -1;
+    }
+    
+    //int file_size = inodes.i_size; // get file size
     int num_blocks = inodes.i_blocks; // get number of blocks
-    /* if file size greater than 4096, 2nd direct blk is need. 
+    
+    /* if file size greater than 4096, 2nd direct blk is needed to read. 
      * if greater than 8192, indirect blk is needed.
      * but I dont know how to implement yet, so just read the 1st direct blk here */
-    int datablk_offset = inodes.direct_blk[0]; // 1st direct datablk
+    //int datablk_offset = inodes.direct_blk[0]; // offset of 1st direct datablk
     
     /* seek to data blk according to offset parameter */
-    lseek(fd, datablk_offset+offset, SEEK_SET); // seek to data blk which storing real data
+    lseek(fd, offset, SEEK_SET); // seek to data blk which storing real data
     
-    /* read to buf based on parameter "count" */
-    ret = read(fd, buf, count); // read the from the offset to buf
+    /* read to buf based on parameter "count" */   
+    ret = read(fd, buf, count); // read the content to buf    
+    //printf("str: %s",buf);
     
-    close(fd);
-    /* return 0 or -1 */
+    /* return ret */
     if(ret != count){ // if read failed       
         perror("failed to read");        
         return -1;    // return -1  
-    } else {
-        return count; // read nothing
-    }
+    }   
+    
+    close(fd);
+    return ret; // num of byte that had read
+    
+    //return 0;
 }
 
 //int main(){
---
+++ sfs_user_commands/cat_t.c
@@ -0,0 +1,57 @@
+#include <stdio.h>     // for printf
+#include <time.h>      // for time_t
+#include <sys/types.h> // for open/read/write
+#include <sys/stat.h>  // for open/read/write
+#include <fcntl.h>     // for open/read/write
+#include <unistd.h>    // for lseek
+#include <string.h>    // for strlen
+#include <stdlib.h>    // for malloc
+
+#include "../sfs.h"    // SFS structures provided by ar sir
+#include "../sfs_functions/sys_call.h" // include self implement open_t, read_t, write_t
+
+int main(int argc, char *argv[]){
+    if(argc!=2){
+        printf("usage: cat_t <file>\n");
+        return -1;
+    }
+
+    /* get the inode number of the input path */
+    char *path = argv[1];
+    if(path[0]!='/'){
+        printf("please use absolute path\n");
+        return -1;
+    }
+    
+    int inode_num = open_t(path, 2); // get the inode number
+    //printf("inode#%d will be read for disply the file content\n",inode_num);
+    
+    /* open HD */
+    int fd = open("../HD",O_RDONLY);
+    
+    /* read out that inode info and pass those info to read_t */
+    ssize_t ret=0; // get bytes from read/write 
+    struct inode inodes = {};
+    int inode_offset = INODE_OFFSET+sizeof(struct inode)*inode_num; // get the offset of that inode number
+    
+    lseek(fd, inode_offset, SEEK_SET); // goto offset of that inode
+    ret = read(fd, &inodes, sizeof(struct inode)); // read the inode to var inodes
+    
+    int data_offset = inodes.direct_blk[0]; // get the datablk offset of real data
+    int file_size = inodes.i_size; // file size in bytes 
+    char buf[file_size]; // buffer for store the content    
+
+    ret = read_t( inode_num, data_offset, &buf, file_size);
+    if(ret!=file_size){
+        perror("failed to read the file...");
+        return -1;
+    }
+    
+    /* the goal of cat_t. after read_t, print out the file content */
+    for(int i=0; i<file_size; i++){ // print char by char
+        printf("%c",buf[i]);
+    }
+    
+    close(fd);
+    return 0;
+}
