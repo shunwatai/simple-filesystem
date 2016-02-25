b5925dc4371746d7bfc6ba3abd21a3b7cb669081
shunwatai <shunwatai@gmail.com>
2016-02-25 23:28:54 +0800

    working on read indirect blk, still in development. NOT TEST YET

--- sfs_functions/read_t.c
+++ sfs_functions/read_t.c
@@ -44,11 +44,53 @@ int read_t( int inode_number, int offset, void *buf, int count){
      * but I dont know how to implement yet, so just read the 1st direct blk here */
     //int datablk_offset = inodes.direct_blk[0]; // offset of 1st direct datablk
     
+    
     /* seek to data blk according to offset parameter */
     lseek(fd, offset, SEEK_SET); // seek to data blk which storing real data
+    char tmpbuf[sizeof(buf)]; // store the read string temporary
     
-    /* read to buf based on parameter "count" */   
-    ret = read(fd, buf, count); // read the content to buf    
+    int remainsize = count; // for count the remain size of file
+    /* read 1st directblk */
+    if(count<=BLOCK_SIZE){ // only one direct blk
+        /* read to buf based on parameter "count" */   
+        ret = read(fd, buf, count); // read the content to buf   
+    }
+    else{ // read 4k blk sin in 1st directblk
+        ret = read(fd, &tmpbuf, BLOCK_SIZE); // read out the 4096 bytes of content to tmpbuf
+        strncpy(buf, &tmpbuf, BLOCK_SIZE);   // copy tmpbuf to buf
+        remainsize = remainsize - BLOCK_SIZE; // count the remaining size to read
+    }
+    
+    /* read 2nd directblk */
+    if(count>BLOCK_SIZE && remainsize<BLOCK_SIZE){
+        lseek(fd, inodes.direct_blk[1], SEEK_SET); // goto 2nd direct blk
+        ret += read(fd, &tmpbuf, remainsize); // read 4096 to tmpbuf
+        strncat(buf, &tmpbuf, remainsize);
+    }
+    if(remainsize>BLOCK_SIZE){ // remaining size still greater than 4096
+        lseek(fd, inodes.direct_blk[1], SEEK_SET); // goto 2nd direct blk
+        ret += read(fd, &tmpbuf, BLOCK_SIZE); // read 4096 to tmpbuf
+        strncat(buf, &tmpbuf, BLOCK_SIZE);
+        remainsize = remainsize - BLOCK_SIZE; // count the remaining size to read
+    }
+    
+    /* indirectblk needed */
+    if(num_blocks>2){ // this inode used more than 2 blk
+        int ptr_offset=0;
+        for(int i=0; i<num_blocks-2; i++){
+            lseek(fd, inodes.indirect_blk, SEEK_SET); // goto indirectblk
+            read(fd, &ptr_offset, sizeof(int)+i*sizeof(int)); // read the offset of indirectblk ptr
+            lseek(fd, ptr_offset, SEEK_SET); // go to offset of that datablk
+            if(remainsize<BLOCK_SIZE){ // if less than 4k,
+                ret += read(fd, &tmpbuf, remainsize); // just read the remaining
+                strncat(buf, &tmpbuf, remainsize); // concat it to buf and break
+                break;
+            }
+            ret += read(fd, &tmpbuf, BLOCK_SIZE); 
+            strncat(buf, &tmpbuf, BLOCK_SIZE);
+        }
+    }
+    
     //printf("str: %s",buf);
     
     /* return ret */
