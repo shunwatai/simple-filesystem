b4e0f950ce0e465f7ae171e0a54f2e566ae93b1c
shunwatai <shunwatai@gmail.com>
2016-02-26 21:38:35 +0800

    merge mkdir_t to open_t

--- sfs_functions/sys_call.h
+++ sfs_functions/sys_call.h
@@ -6,11 +6,12 @@ extern int open_t(const char *path, int flags);
 extern int print_sb(struct superblock);
 extern int print_inode(struct inode);
 extern int split_path(char*, char**);
+extern int makedir(int , struct superblock *, struct inode *, char *, int );
 
 /* functions in read_t.c */
-int read_t( int, int, void*, int );
+extern int read_t( int, int, void*, int );
 
 /* functions in write_t.c */
-int write_t( int, int, void*, int );
+extern int write_t( int, int, void*, int );
 
 #endif /* SYS_CALL_H_ */
